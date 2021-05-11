d65a2ca56d089801581c9b75bed514c3c6bb37d0
woonmgi10 <woonmgi10@gmail.com>
2021-05-11 20:54:38 +0900

    gd

---
+++ src/main/java/org/spoiler/board/free/domain/FreeMemberVO.java
@@ -0,0 +1,12 @@
+package org.spoiler.board.free.domain;
+
+public class FreeMemberVO {
+
+	private String userId;
+	private String userPw;
+	private String name;
+	private String email;
+	private String birth;
+	private int phone;
+	private String gender;
+}
--- src/main/webapp/WEB-INF/views/customLogin.jsp
+++ src/main/webapp/WEB-INF/views/customLogin.jsp
@@ -102,7 +102,6 @@
   
   function enterkey() {
       if (window.event.keyCode == 13) {
-
            // 엔터키가 눌렸을 때 실행할 내용
     	  $("form").submit();
       }
